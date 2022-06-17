3961547209a33d64e708dec9c2451fa2fbf5b0e1
benjimola <benjamin.ayt@hotmail.co.uk>
2022-06-17 08:45:56 +0100

    Css
    Fixed some css issues

---
+++ 1337/Models/MovieSource.cs
@@ -0,0 +1,12 @@
+﻿namespace _1337.Models
+{
+    public class MovieSource
+    {
+
+        public int? Size { get; set; }
+        public string? Language { get; set; }
+        public string? Quality { get; set; }
+        public string? Category { get; set; }
+        public string? Hash { get; set; }
+    }
+}
